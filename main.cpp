8e59e07c5a5355499c170efe018c08041449caf0
NahYoungLyroal <ear062311@gamil.com>
2023-10-30 09:25:06 +0900

    아침 과제.

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+void DrawMap(char Text1, char Text2);
+
+int main()
+{
+	
+	DrawMap('1', '0');
+
+}
+
+void DrawMap(char Text1, char Text2)
+{
+	Text1 = '1';
+	Text2 = '2';
+
+	for (int Y = 0; Y < 10; Y++)
+	{
+		for (int X = 0; X < 10; X++)
+		{
+			if (X == 0 || Y == 0 || X == 9 || Y == 9 || X == 4 || Y == 4)
+			{
+				cout << Text1;
+			}
+			else
+			{
+				cout << Text2;
+			}
+
+		}
+		cout << endl;
+
+	}
+}
